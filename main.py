5a89fd7b46190435a8af971173596bed54b24483
Kouakou-joel <jojomignon5612@gmail.com>
2025-01-31 19:03:52 +0000

    _commit_modif

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, HTTPException
 from models import Todo, Todo_pydantic, TodoIn_pydantic
 from tortoise.contrib.fastapi import HTTPNotFoundError, register_tortoise
 from pydantic import BaseModel
+from typing import List
 
 class Messa(BaseModel):
     message: str
@@ -13,7 +14,7 @@ def default():
     return {"message": "Bonjour todo"}
 
 @app.post("/todo", response_model=Todo_pydantic)
-async def create(todo: TodoIn_pydantic): 
+async def create(todo = TodoIn_pydantic):  
     obj = await Todo.create(**todo.dict(exclude_unset=True))
     return await Todo_pydantic.from_tortoise_orm(obj)
 
@@ -23,7 +24,7 @@ async def get_todo(id: int):
      return await Todo_pydantic.from_queryset_single(Todo.get(id=id))
 
 @app.put("/todo/{id}", response_model=Todo_pydantic)
-async def update_todo(id: int, todo: TodoIn_pydantic):
+async def update_todo(id: int, todo = TodoIn_pydantic):
     updated_count = await Todo.filter(id=id).update(**todo.dict(exclude_unset=True))
     if not updated_count:
         raise HTTPException(status_code=404, detail="Todo non trouvé")
@@ -35,6 +36,12 @@ async def delete_todo(id: int):
     if not deleted_count:
         raise HTTPException(status_code=404, detail="Todo non trouvé")
     return Messa(message="Supprimé avec succès")
+
+@app.get("/todos/", response_model=List[Todo_pydantic])
+async def get_todos():
+    return await Todo_pydantic.from_queryset(Todo.all())
+
+
 
 register_tortoise(
     app,
